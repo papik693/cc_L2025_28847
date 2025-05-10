293265d6ee4d6a464a0820be777d54163fa69ac9
Patryk Heyn <pheyn@edu.cdv.pl>
2025-05-10 13:06:55 +0200

    lab2 - first version

---
+++ 250510_lab2/CDV/Cdv.Domain/Entities/PersonEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Cdv.Domain.Entities;
+
+
+public class PersonEntity
+{
+    public int Id {get; set; }
+    public string FirstName {get; set; }
+    public string LastName {get; set; }
+    
+}
---
+++ 250510_lab2/CDV/Cdv.Functions/PeopleFn.cs
@@ -0,0 +1,90 @@
+﻿using Cdv.Domain.DbContext;
+using Cdv.Functions.Dto;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cdv.Functions;
+
+public class PeopleFn
+{
+    private readonly ILogger<PeopleFn> _logger;
+    private readonly PeopleDbContext db;
+
+    public PeopleFn(ILogger<PeopleFn> logger, PeopleDbContext db)
+    {
+        _logger = logger;
+        this.db = db;
+    }
+
+    [Function("PeopleFn")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function,
+            "get", "post", "put", "delete")]
+        HttpRequest req)
+    {
+        switch (req.Method)
+        {
+            case "POST":
+                var person = CreatePerson(req);
+                return new OkObjectResult(person);
+            case "GET":
+                var idExist = req.Query.Any(w => w.Key == "id");
+
+                if (idExist)
+                {
+                    var personId = req.Query.First(w => w.Key == "id").Value;
+                    int id = Int32.Parse(personId.First());
+                    return new OkObjectResult(FindPerson(id));
+                }
+
+                var people = GetPeople(req);
+                return new OkObjectResult(people);
+            case "DELETE":
+                DeletePerson(req);
+                return new OkResult();
+            case "PUT":
+                UpdatePerson(req);
+                return new OkResult();
+        }
+
+        throw new NotImplementedException("Uknown method");
+    }
+
+    private List<PersonDto> GetPeople(HttpRequest req)
+    {
+        var people = db.People.ToList();
+        return people.Select(s => new PersonDto
+        {
+            Id = s.Id,
+            FirstName = s.FirstName,
+            LastName = s.LastName,
+        }).ToList();
+    }
+
+    private void UpdatePerson(HttpRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DeletePerson(HttpRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    private PersonDto FindPerson(int personId)
+    {
+        var person = db.People.First(w=>w.Id==personId);
+        return new PersonDto
+        {
+            Id = person.Id,
+            FirstName = person.FirstName,
+            LastName = person.LastName,
+        };
+    }
+
+    private PersonDto CreatePerson(HttpRequest req)
+    {
+        throw new NotImplementedException();
+    }
+}
