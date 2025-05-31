61870d568700870fbdf4889edc005a2556cb16ae
Patryk <patrykheyn@gmail.com>
2025-05-31 14:07:22 +0200

    remove unnecessary comments

--- azure_project/deploy-function-app.sh
+++ azure_project/deploy-function-app.sh
@@ -11,27 +11,21 @@ RESOURCE_GROUP=${2:-cdv}
 
 cd function_app
 
-# Clean up previous build
 rm -rf .build
 mkdir -p .build
 
-# Copy function files to build directory
 cp -r api .build/
 cp requirements.txt .build/
 
-# Create and activate virtual environment
 python3 -m venv .build/.venv
 source .build/.venv/bin/activate
 
-# Install dependencies
 cd .build
 pip install --target=. -r requirements.txt
 
-# Create deployment package
 rm -f ../../function_app.zip
 zip -r ../../function_app.zip .
 
-# Clean up and deploy
 cd ../..
 echo "Deploying to Azure..."
 az functionapp deployment source config-zip \
@@ -39,6 +33,5 @@ az functionapp deployment source config-zip \
   -n $FUNCTION_APP_NAME \
   --src function_app.zip
 
-# Clean up build artifacts
 rm -f function_app.zip
 rm -rf function_app/.build 
